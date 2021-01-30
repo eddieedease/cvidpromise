93dd709073130650db590efa57f0e5f41e6e2cda
Eddie <eddie@edease.nl>
2021-01-30 02:16:34 +0100

    listupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,20 @@ export class AppComponent {
 
 
     this.checks = [
-      'Ik weet niet alles en dat is gewoon helemaal oké',
-      'Ik respecteer andere mensen, hun mening en eigendommen',
-      'Ik ben trots op ons land, ook al worden er soms fouten gemaakt',
+      'Ik weet niet alles, en weet je wat? Dat is gewoon helemaal oké',
+      'Ik respecteer andere mensen, hun mening en hun eigendommen',
+      'Ik besef dat het voor veel mensen (waaronder misschien ikzelf) een erg zware tijd is',
+      'Ik ben trots op ons land, ook al worden er soms fouten gemaakt. Er is zoveel om trots op te zijn',
+      'Ik besef dat deze nare coronatijd ook weer voorbij gaat',
+      'Ik heb niet het idee dat ik de hele bevolking moet behoeden van wat dan ook',
       'Hoe rot deze tijd ook is, ik probeer er het beste van te maken',
-      'Ik accepteer mindere dagen, de boog kan niet altijd gespannen staan',
+      'Ik dring niemand mijn mening op',
+      'Ik accepteer de mindere dagen, de boog kan niet altijd gespannen staan',
+      'Ik laat mezelf niet zomaar iets aanpraten, maar ga ook niet overal iets achter zoeken',
       'Ik ben bewust dat ik niet de enige ben met onzekerheid en angstgevoelens',
-      'Ik ga negatieve energieslopende situaties uit de weg'
+      'Ik ga negatieve energieslopende perikelen, on- en offline, uit de weg',
+      'Ik veroordeel zinloos geweld, maar geweld in het algemeen',
+      'Ik ben bereid hier beter uit te komen, dat dat ik erin ging'
     ];
   }
 }
